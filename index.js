50a1e240eecab8065f4d01de3dd796289c2e0fda
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-01-03 14:19:34 +0000

    S

--- index.js
+++ index.js
@@ -8,11 +8,13 @@
 				$("#displayPhonebookLength").html(contacts.length + ' contacts found');
 				for (var i = 0; i < contacts.length; i++) {
 					console.log(contacts[i].id + " - " + contacts[i].displayName);
+					var contactName = contacts[i].displayName;
 					for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
 						var phone = contacts[i].phoneNumbers[j];
 						// var det = $("#displayPhonebook").html();
 						// console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
-						$("#displayPhonebook").append("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
+						var details = contactName + " (" + phone.number + ")";
+						$("#displayPhonebook").append(details);
 					}
 				}
 			}, function(error) {
@@ -22,3 +24,4 @@
 	};
 
 })();
+
